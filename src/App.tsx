d75e2e20a0bf0c5f004186278ac1354203e38801
Matías Méndez <matiasmendez722@gmail.com>
2026-04-17 03:04:11 -0300

    Creación de componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
 
+import { NavBar } from './components/NavBar'
 
 
 
 function App() {
   return (
     <BrowserRouter>
+      <NavBar />
 
       <Routes>
-        <Route path="/" element={<Home />} />
       </Routes>
-
+      
     </BrowserRouter>
   )
 }
