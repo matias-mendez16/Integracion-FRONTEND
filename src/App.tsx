ebd9784e9b1e5a236d73bbc238c65193cbc4665d
Matías Méndez <matiasmendez722@gmail.com>
2026-04-17 02:52:02 -0300

    Creacion de Landing Page - FAQ

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { FAQ } from './components/FAQ'
+
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/faq" element={<FAQ />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
