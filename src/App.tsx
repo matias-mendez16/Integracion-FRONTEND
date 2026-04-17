e31fb1ffe65c69dee889a992dffca5b37ed9de32
Matías Méndez <matiasmendez722@gmail.com>
2026-04-17 03:06:03 -0300

    Creación de componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { NavBar } from './components/NavBar'
+import { Footer } from './components/Footer'
 
 
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
 
       <Routes>
       </Routes>
-      
+
+      <Footer />
     </BrowserRouter>
   )
 }
