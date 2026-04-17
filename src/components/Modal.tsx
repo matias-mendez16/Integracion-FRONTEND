a80c83e870d705ee3609091fe8543827369cb908
Cristian Falcone <cristianfalcone@hotmail.com>
2026-04-17 00:15:05 -0300

    test de inversor con estilos y var globales generales (con formulario prueba)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,24 +1,18 @@
 import React, { type ReactNode } from 'react';
 import "./Modal.css"
 
-// 1. Definimos qué necesita el modal para funcionar
 interface ModalProps {
-  estaAbierto: boolean;        // Controla si se ve o no
-  cerrar: () => void;    // Función para cerrar
-  children: ReactNode;    // El contenido (texto, otros componentes, etc.)
+  estaAbierto: boolean;
+  cerrar: () => void;
+  children: ReactNode;
 }
 
 const Modal: React.FC<ModalProps> = ({ estaAbierto, cerrar, children }) => {
   if (!estaAbierto) return null;
 
   return (
-    <div 
-      className="capaFondo"
-      onClick={cerrar} // Cierra al hacer clic fuera
-    >
-      <div className="contModal"
-        onClick={(e) => e.stopPropagation()} // Evita cerrar al hacer clic dentro
-      >
+    <div className="capaFondo" onClick={cerrar}>
+      <div className="contModal" onClick={(e) => e.stopPropagation()}>
         <div className="contBotonX">
           <button onClick={cerrar} className="botonCerrarX">
             ✕
@@ -30,7 +24,6 @@ const Modal: React.FC<ModalProps> = ({ estaAbierto, cerrar, children }) => {
         <div id='contBotonCerrar'>
           <button onClick={cerrar} className="botonCerrar">Cerrar</button>            
         </div>
-
       </div>
     </div>
   );
