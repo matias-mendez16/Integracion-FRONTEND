b6e1357483466ab258d7226551bdf51c1a5b8afe
Cristian Falcone <cristianfalcone@hotmail.com>
2026-04-18 20:15:36 -0300

    test inversor probando

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import './App.css'
-import { BrowserRouter, Routes} from 'react-router-dom'
-
-import { NavBar } from './components/NavBar'
-import TestInversor from './components/TestInversor'
-
-
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-
-      <Routes>
-      </Routes>
-          <TestInversor/>
-    </BrowserRouter>
+    <>
+    </>
   )
 }
 
