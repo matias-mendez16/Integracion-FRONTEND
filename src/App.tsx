c66a1d28565a72285413a4e7df9a25e28a3fc58e
Matías Méndez <matiasmendez722@gmail.com>
2026-04-18 19:46:56 -0300

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './components/Home'
-
-
-
 
 function App() {
   return (
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-
-    </BrowserRouter>
+    <>
+    </>
   )
 }
 
