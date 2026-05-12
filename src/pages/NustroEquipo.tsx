79bc1bf70586c09ad9892d56066f5ae97118c7ea
milena-martinez04 <mariamilenamartinez04@gmail.com>
2026-05-12 11:13:33 -0300

    Page Nuestro Equipo

--- src/pages/NustroEquipo.tsx
+++ src/pages/NustroEquipo.tsx
@@ -1,11 +1,5 @@
 import styles from './NuestroEquipo.module.css'
-
-const equipo = [
-    { id: 1, nombre: "Roberto Garcia", img: "https://randomuser.me/api/portraits/men/1.jpg", descripcion: "Desarrollador Full Stack"},
-    { id:2 , nombre: "Juan López", img: "https://randomuser.me/api/portraits/men/10.jpg", descripcion: "Diseñador UX" },
-    { id:3, nombre: "Carlos Rodríguez", img: "https://randomuser.me/api/portraits/men/20.jpg", descripcion: "Analista de Datos" },
-    { id:4, nombre: "Ana Martínez", img: "https://randomuser.me/api/portraits/women/2.jpg", descripcion: "Especialista en Marketing" }
-]
+import { CardPersona } from '../components/CardPersona'
 
 export function NuestroEquipo() {
 
@@ -21,16 +15,7 @@ export function NuestroEquipo() {
             <h4 className={styles.h4}>-Innovación: La mejor tecnología, bajo estrícto cumplimiento normativo.</h4>
             <h4 className={styles.h4}>-Educacion continua: Herramientas para el crecimiento del inversor.</h4>
 
-            <div className={styles.equipo}>
-
-                {equipo.map((persona) => (
-                    <div key={persona.id} className={styles.card}>
-                        <img className={styles.img} src={persona.img} alt={persona.nombre} />
-                        <p className={styles.nombre}>{persona.nombre}</p>
-                        <p className={styles.descripcion}>{persona.descripcion}</p>
-                    </div>
-                ))}
-            </div>
+            <CardPersona />
         </div>
     )
 }
